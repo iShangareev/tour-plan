69b51ff2ce40cd7c978f4472c6cf2210d0460151
iShangareev <ilnarshan@gmail.com>
2020-07-21 13:36:42 +0400

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".sliper-button--next",
+    prevEl: ".sliper-button--prev",
+  },
+  // effect: "cube",
+});
