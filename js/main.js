ee9c9a0b249749d734ccb3c0d3520010d5faf92a
iShangareev <ilnarshan@gmail.com>
2020-07-28 12:12:40 +0400

    create : mobile menu

--- js/main.js
+++ js/main.js
@@ -31,3 +31,11 @@ var reviewsSlider = new Swiper(".reviews-slider", {
     onlyInViewport: false,
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик клак");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
